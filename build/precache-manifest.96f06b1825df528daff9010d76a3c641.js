75e397374594fd50fd661756c0330e265a99faa4
Vigen Abrahamyan <vigen.abrahamyan@fluxtechs.com>
2019-04-28 22:42:49 +0400

    adde build folder for github pages

---
+++ build/precache-manifest.96f06b1825df528daff9010d76a3c641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657581f0d82b90fde3d95b2db85136b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4847007a44f5f53e10d",
+    "url": "/static/css/main.9d24f169.chunk.css"
+  },
+  {
+    "revision": "f2283983dcd26239fcbc",
+    "url": "/static/js/2.98e34a1e.chunk.js"
+  },
+  {
+    "revision": "e4847007a44f5f53e10d",
+    "url": "/static/js/main.ca8e29e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
